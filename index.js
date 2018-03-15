d14724037ec794ac98255bfe5a44c633b06c137b
Markus Wolf <markus.wolf@magicline.de>
2018-03-15 14:04:42 +0100

    feat: initial version

---
+++ index.js
@@ -0,0 +1,53 @@
+const path = require('path');
+const fs = require('fs');
+
+class HtmlWebpackExcludeEmptyAssetsPlugin {
+  apply(compiler) {
+    compiler.plugin("compilation", compilation => {
+      compilation.plugin(
+        "html-webpack-plugin-alter-asset-tags",
+        (htmlPluginData, callback) => {
+          const result = this.processAssets(htmlPluginData, compilation);
+          if (callback) {
+            callback(null, result);
+          } else {
+            return Promise.resolve(result);
+          }
+        }
+      );
+    });
+  }
+
+  processAssets(pluginData, compilation) {
+    const base = JSON.parse(pluginData.plugin.assetJson)[0];
+    const head = pluginData.head.filter(
+      tag =>
+        !Boolean(tag.attributes) ||
+        !this.isEmpty(tag.attributes.src || tag.attributes.href, base, compilation)
+    );
+    const body = pluginData.body.filter(
+      tag =>
+        !Boolean(tag.attributes) ||
+        !this.isEmpty(tag.attributes.src || tag.attributes.href, base, compilation)
+    );
+
+    return {
+      head,
+      body,
+      plugin: pluginData.plugin,
+      chunks: pluginData.chunks,
+      outputName: pluginData.outputName
+    };
+  }
+
+  isEmpty(assetPath, base, compilation) {
+    const rel = assetPath.substr(base.length);
+    const source = compilation.assets[rel];
+    if (source && source.size) {
+      return source.size() === 0;
+    }
+    return false;
+  }
+}
+
+module.exports = HtmlWebpackExcludeEmptyAssetsPlugin;
